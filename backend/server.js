a905b775817093e3a0abe4cf9823bd9b1db2f5dc
MahirKaushik <kaushik.mahir25@gmail.com>
2025-05-25 20:09:41 +0530

    added .env file and understand  middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
+require('dotenv').config()
+
 const express=require('express')
 
 const app=express()
 
-app.listen(4000,()=>{
-    console.log('listening on port 4000')
+
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+
+
+//requests get post put patch or delete
+app.get('/',(req,res)=>{
+    res.json('hello')
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log('listening on port',process.env.PORT)
 })
