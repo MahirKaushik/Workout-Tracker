7fc34c8b692b3d5e425027b82eec4c3bc9be6787
MahirKaushik <kaushik.mahir25@gmail.com>
2025-06-12 00:08:51 +0530

    Made Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages & components
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
+
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home=()=>{
+    return(
+        <div className="home">
+            Rom Rom
+        </div>
+    )
+}
+
+export default Home
